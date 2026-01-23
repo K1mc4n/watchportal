3f0f0123b5b887bdaadcb205cf8009ac35f8bedb
Watchcoin Ventures <156217571+K1mc4n@users.noreply.github.com>
2026-01-24 00:36:39 +0700

    Update neynar.ts

--- src/lib/neynar.ts
+++ src/lib/neynar.ts
@@ -1,17 +1,9 @@
 import { NeynarAPIClient } from "@neynar/nodejs-sdk";
 
-export const NEYNAR_API_KEY = process.env.NEYNAR_API_KEY as string;
-export const NEYNAR_CLIENT_ID = process.env.NEYNAR_CLIENT_ID as string;
+const apiKey = process.env.NEYNAR_API_KEY;
 
-if (!NEYNAR_API_KEY) {
+if (!apiKey) {
   throw new Error("NEYNAR_API_KEY is missing");
 }
 
-let client: NeynarAPIClient | null = null;
-
-export function getNeynarClient() {
-  if (!client) {
-    client = new NeynarAPIClient(NEYNAR_API_KEY);
-  }
-  return client;
-}
+export const neynarClient = new NeynarAPIClient(apiKey);
